c17a22163ad52ef53398630615732fc850d8a645
Julian Minder <>
2018-09-26 23:06:10 +0200

    typo

--- SafeTRex/car.py
+++ SafeTRex/car.py
@@ -1,36 +1,40 @@
-#import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 import sys, getopt
 
 
 class Servo(object):
     """Ansteuerung von Servos per PWM"""
+
     def __init__(self, gpio, min=0, max=180):
-        if min>max:
-            tmp=min
-            min=max
-            max=tmp
-        if min<0:
-            min=0
-        if max>180:
-            max=180
-	    GPIO.setmode(GPIO.BCM)
-	    GPIO.setup(gpio, GPIO.OUT)
-        self.io = GPIO.PWM(gpio, 50) # 50 Hz
+        if min > max:
+            tmp = min
+            min = max
+            max = tmp
+        if min < 0:
+            min = 0
+        if max > 180:
+            max = 180
+
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(gpio, GPIO.OUT)
+        self.io = GPIO.PWM(gpio, 50)  # 50 Hz
         self.io.start(7.5)
-        self.min=min
-        self.max=max
+        self.min = min
+        self.max = max
 
     def hello(self, name):
-        print('Servo '+name+' from '+str(self.min)+' to '+str(self.max))
+        print('Servo ' + name + ' from ' + str(self.min) + ' to ' + str(self.max))
 
-
-    def go(self,angle):
-        if angle<self.min: angle=self.min
-        elif angle>self.max: angle=self.max
-        self.io.ChangeDutyCycle(float(angle)/18+2.5)
+    def go(self, angle):
+        if angle < self.min:
+            angle = self.min
+        elif angle > self.max:
+            angle = self.max
+        self.io.ChangeDutyCycle(float(angle) / 18 + 2.5)
         time.sleep(0.2)
-        print('Servo angle '+str(angle))
+        print('Servo angle ' + str(angle))
 
     def close(self):
         self.io.stop()
@@ -39,11 +43,12 @@ class Servo(object):
 
 class ServoCar(object):
     """Ansteuerung eines Modellautos mit Steuer und Speed"""
+
     def __init__(self):
         self.__steer = Servo(23, 45, 135)
-        self.__steerFactor = float(self.__steer.max)/100
+        self.__steerFactor = float(self.__steer.max) / 100
         self.__speed = Servo(24)
-        self.__speedFactor = float(self.__speed.max)/255/4.5
+        self.__speedFactor = float(self.__speed.max) / 255 / 4.5
 
     def hello(self):
         print('Hello from car')
@@ -51,11 +56,11 @@ class ServoCar(object):
         self.__speed.hello('Geschwindigkeit')
 
     def steer(self, value):
-        self.__steer.go(90+self.__steerFactor*float(value))
+        self.__steer.go(90 + self.__steerFactor * float(value))
 
     def speed(self, value):
         print("Set Speed to" + value)
-        self.__speed.go(90+self.__speedFactor*float(value))
+        self.__speed.go(90 + self.__speedFactor * float(value))
 
 
 class CarStateMachine():
@@ -64,8 +69,8 @@ class CarStateMachine():
         self.setRUN(30)
         self._car = ServoCar()
 
-    def setState(self, state, tempo = -1):
-        #RUN
+    def setState(self, state, tempo=-1):
+        # RUN
         if state == "RUN":
             self.setRUN(tempo)
 
@@ -78,7 +83,3 @@ class CarStateMachine():
     def setSTOP(self):
         self._car.speed(0)
         self.__state = ("STOP", 0)
-
-
-
-
