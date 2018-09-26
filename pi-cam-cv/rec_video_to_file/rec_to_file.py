e9212defb9c1b36e24f3fd00c792ac3ad7a6f916
Lorenz Hänggi <lolo8304@gmail.com>
2018-09-26 14:36:05 +0200

    update format

--- pi-cam-cv/rec_video_to_file/rec_to_file.py
+++ pi-cam-cv/rec_video_to_file/rec_to_file.py
@@ -1,7 +1,13 @@
 import picamera
+import argparse
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--len", required=True, type=int,
+	help="len of video")
+args = vars(ap.parse_args())
+
 
 with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)
-    camera.start_recording('my_video.h264')
-    camera.wait_recording(60)
+    camera.start_recording('my_video.mjpeg')
+    camera.wait_recording(args["len"])
     camera.stop_recording()
