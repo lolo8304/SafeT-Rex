60f500bf746149ccd342e9f15b64be9e9f001cfa
Lorenz Hänggi <lolo8304@gmail.com>
2018-09-26 15:35:02 +0200

    new extension

--- pi-cam-cv/rec_video_to_file/rec_to_file.py
+++ pi-cam-cv/rec_video_to_file/rec_to_file.py
@@ -5,12 +5,17 @@ ap.add_argument("-l", "--len", required=True, type=int,
 	help="len of video")
 ap.add_argument("-f", "--format", required=False, default="h264",
 	help="video format")
+ap.add_argument("-e", "--ext", required=False, default="",
+	help="extension")
 args = vars(ap.parse_args())
 
 
 with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)
     filename = 'saft-rex.'+args["format"]
+    if (args["ext"] == "") {
+        filename = filename + "." + ext
+    }
     print ('recording ',args["len"], 's and save to ',filename)
 
     camera.start_recording(filename)
