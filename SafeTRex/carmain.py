2406ae31431b309b519640fac114a1f8a359e4d6
Lorenz Hänggi <lolo8304@gmail.com>
2018-09-27 14:15:43 +0200

    remote client

--- SafeTRex/carmain.py
+++ SafeTRex/carmain.py
@@ -2,11 +2,11 @@ from car_client import *
 import time
 import argparse
 ap = argparse.ArgumentParser()
-ap.add_argument("-s", "--speed", required=False, type=int, default=False,
+ap.add_argument("-s", "--speed", required=False, type=int,
 	help="speed mode")
-ap.add_argument("-t", "--stop", required=False, type=int, default=False,
+ap.add_argument("-t", "--stop", required=False, type=int,
 	help="speed mode")
-ap.add_argument("-c", "--steer", required=False, type=int, default=False,
+ap.add_argument("-c", "--steer", required=False, type=int,
 	help="steer mode")
 args = vars(ap.parse_args())
 
@@ -25,7 +25,5 @@ if (args["stop"] is not None):
   pass
   #__driver.setSTOP()
 
-print("sleep to close")
-time.sleep(2)
 #__car.stop()
 __driver.close()
