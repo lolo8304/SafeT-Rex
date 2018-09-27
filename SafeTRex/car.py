7bca57d48f7b22adbb3a056b1266e3885d081129
Lorenz Hänggi <lolo8304@gmail.com>
2018-09-27 12:55:26 +0200

    update car

--- SafeTRex/car.py
+++ SafeTRex/car.py
@@ -6,7 +6,7 @@ import sys
 class Servo(object):
     """Ansteuerung von Servos per PWM"""
 
-    def __init__(self, gpio, hz=50, min=0, max=180):
+    def __init__(self, gpio, hz=50, start=90, min=0, max=180):
         if min > max:
             tmp = min
             min = max
@@ -20,14 +20,14 @@ class Servo(object):
         GPIO.setup(gpio, GPIO.OUT)
         GPIO.setwarnings(False)
         self.io = GPIO.PWM(gpio, hz)  # 50 Hz
-        self.io.start(7.5)
+        self.io.start(start)
         self.min = min
         self.max = max
 
     def hello(self, name):
         print('Servo ' + name + ' from ' + str(self.min) + ' to ' + str(self.max))
 
-    def go(self, angle):
+    def angle(self, angle):
         if angle < self.min:
             angle = self.min
         elif angle > self.max:
@@ -35,6 +35,15 @@ class Servo(object):
         self.io.ChangeDutyCycle(float(angle) / 18 + 2.5)
         #time.sleep(0.2)
         print('Servo angle ' + str(angle))
+
+ def pulse(self, angle):
+        if angle < self.min:
+            angle = self.min
+        elif angle > self.max:
+            angle = self.max
+        self.io.ChangeDutyCycle(float(angle))
+        #time.sleep(0.2)
+        print('Servo pulse ' + str(angle))
 
     def close(self):
         self.io.stop()
@@ -46,11 +55,11 @@ class ServoCar(object):
 
     def __init__(self):
         # Steuerung -100 ... 100
-        self.__steer = Servo(23, 50, 45, 135)
+        self.__steer = Servo(23, 50, 7.5, 45, 135)
         self.__steerFactor = float(self.__steer.max) / 100
 
         # Geschwindigkeit 0...100
-        self.__speed = Servo(24, 10, 0, 100)
+        self.__speed = Servo(24, 10, 0, 0, 100)
         self.__speedFactor = 0.25
 
     def hello(self):
@@ -60,11 +69,11 @@ class ServoCar(object):
 
     def steer(self, value):
         print("CAR: steer to " + str(value))
-        self.__steer.go(90 + self.__steerFactor * float(value))
+        self.__steer.angle(90 + self.__steerFactor * float(value))
 
     def speed(self, value):
         print("CAR: Set Speed to " + str(value))
-        self.__speed.go(self.__speedFactor * float(value))
+        self.__speed.pulse(self.__speedFactor * float(value))
 
 
 class CarStateMachine():
