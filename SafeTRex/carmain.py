a6f47e7cabdd0e9a10dfac2c2a5fce777ba681f6
Lorenz Hänggi <lolo8304@gmail.com>
2018-09-27 13:09:16 +0200

    remove

--- SafeTRex/carmain.py
+++ SafeTRex/carmain.py
@@ -9,14 +9,17 @@ ap.add_argument("-c", "--steer", required=False, type=int, default=False,
 	help="steer mode")
 args = vars(ap.parse_args())
 
-__driver = CarStateMachine()
+#__driver = CarStateMachine()
+__car = ServoCar()
 
 if (args["speed"] > 0):
-  __driver.setRUN(args["speed"])
+  #__driver.setRUN(args["speed"])
+  __car.speed(args["speed"])
 
 if (args["steer"] > 0):
-  __driver.setAngle(args["steer"])
+  #__driver.setAngle(args["steer"])
+  __car.steer(args["steer"])
 
 if (args["stop"] > 0):
-  __driver.setSTOP()
+  #__driver.setSTOP()
 
