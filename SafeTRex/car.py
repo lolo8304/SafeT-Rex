b8b20e51e6e0180531604fac5745ff2098c8013d
Lorenz Hänggi <lolo8304@gmail.com>
2018-09-27 14:10:53 +0200

    remote client

--- SafeTRex/car.py
+++ SafeTRex/car.py
@@ -60,7 +60,7 @@ class ServoCar(object):
 
         # Geschwindigkeit 0...100
         self.__speed = Servo(24, 20, 0, 0, 100)
-        self.__speedFactor = 0.50
+        self.__speedFactor = 0.25
 
     def hello(self):
         print('CAR: Hello from car')
