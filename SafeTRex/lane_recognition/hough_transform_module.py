b340cbb66955cbd56f844bb761b27adc0c2f3175
Lorenz Hänggi <lolo8304@gmail.com>
2018-09-29 22:09:29 +0200

    wrong kalibration

--- SafeTRex/lane_recognition/hough_transform_module.py
+++ SafeTRex/lane_recognition/hough_transform_module.py
@@ -246,8 +246,9 @@ def steering_directionX(intersection_point, left, right, image, defaultW = 0):
 # divide by 8 regions each side, first 2 regions each side - straight
 # value between -45 - 45
 def steering_angle(directionX):
-    directionX100 = max(min(int(directionX * 45), 12), -12)
-    absStraightDistance = int(45 * 2 / 8)
+    maxAngle = 20
+    directionX100 = int(directionX * maxAngle)
+    absStraightDistance = int(maxAngle * 2 / 8)
     isStraight = abs(directionX100) <= absStraightDistance
     if isStraight:
         return "straight", directionX100
