a5d957a900a4c03b220c98dcff6b16e5926ae375
Lorenz Hänggi <lolo8304@gmail.com>
2018-09-29 16:07:19 +0200

    shut of writeing

--- SafeTRex/lanedetector.py
+++ SafeTRex/lanedetector.py
@@ -13,17 +13,17 @@ def releaseVideo(video):
     video.release()
 
 
-def dd(self):
-    if self.__sr.needsRecording():
-    if self.__video is None:
-        name = "safet-rex-recording-"+str(self.__sr.recordingNo())+".h264"
-        print("start frame recording to ", name)
-        (h, w) = image.shape[:2]
-        if os.path.exists(name):
-            os.remove(name)
-        video = cv2.VideoWriter(name, cv2.VideoWriter_fourcc('h','2','6','4'), 10, (w,h))
-        atexit.register(releaseVideo, video)
-    video.write(image)
+# def dd(self):
+#     if self.__sr.needsRecording():
+#     if self.__video is None:
+#         name = "safet-rex-recording-"+str(self.__sr.recordingNo())+".h264"
+#         print("start frame recording to ", name)
+#         (h, w) = image.shape[:2]
+#         if os.path.exists(name):
+#             os.remove(name)
+#         video = cv2.VideoWriter(name, cv2.VideoWriter_fourcc('h','2','6','4'), 10, (w,h))
+#         atexit.register(releaseVideo, video)
+#     video.write(image)
 
 
 class LineDetector():
