aa4b0f1d4e791629cc69b8f2714222b9c878f08e
Lorenz Hänggi <lolo8304@gmail.com>
2018-10-03 00:15:44 +0200

    missing command

--- start.py
+++ start.py
@@ -4,6 +4,8 @@ import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--debug", required=False, type=bool, default=False,
 	help="debug mode")
+ap.add_argument("-x", "--xdebug", required=False, type=bool, default=False,
+	help="X debug mode")
 ap.add_argument("-r", "--recording", required=False, type=int, default=0,
 	help="number to write files accordinly")
 args = vars(ap.parse_args())
