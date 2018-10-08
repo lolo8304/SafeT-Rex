3ca5df620fd66037446a7aef99312b7147ad644c
Lorenz Hänggi <lolo8304@gmail.com>
2018-10-08 20:05:56 +0200

    adapt to use configuration

--- SafeTRex/lanedetector.py
+++ SafeTRex/lanedetector.py
@@ -1,4 +1,7 @@
 from .lane_recognition.hough_transform_module import detect_lane
+from .lane_recognition.hough_transform_module import execute_pipeline_key
+from .lane_recognition.hough_configuration import configurations
+
 import time
 
 def lanedetector(sr, driver):
@@ -11,9 +14,11 @@ class LineDetector():
         self.__sr = sr
         self.__driver = driver
         print("starting lanedetector")
+        self.__image_config = configurations()["axahack2018"]
+
 
     def run(self):
         while(True):
             image  = self.__sr.getCurrentImage()
-            detect_lane(image, self.__sr.isDebug(), self.__sr.isXDebug(), self.__driver)
+            detect_lane(image, self.__image_config, self.__sr.isDebug(), self.__sr.isXDebug(), self.__driver)
             time.sleep(0.5)
